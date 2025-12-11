9289c3efe01de6d95f5fe2d4530d332087697bc1
ابوالفضل سلطانی <158173148+Abolfazlsoltan@users.noreply.github.com>
2025-12-11 21:17:12 +0330

    Create server.js

---
+++ project/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const authRoutes = require("./routes/auth");
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5500",
+    credentials: true
+}));
+
+app.use("/api/auth", authRoutes);
+
+app.listen(3000, () => console.log("Server running on port 3000"));
