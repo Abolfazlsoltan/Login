efa3031fdd38c01eec5b04ddf8874cb1d6ae6965
ابوالفضل سلطانی <158173148+Abolfazlsoltan@users.noreply.github.com>
2025-12-11 21:26:06 +0330

    Update authMiddleware.js

--- project/middleware/authMiddleware.js
+++ project/middleware/authMiddleware.js
@@ -1,1 +1,18 @@
+const jwt = require("jsonwebtoken");
 
+const ACCESS_SECRET = "ACCESS_SECRET_ABC123";
+
+module.exports = (req, res, next) => {
+    const header = req.headers.authorization;
+    if (!header) return res.status(401).json({ error: "توکن نیست" });
+
+    const token = header.split(" ")[1];
+
+    try {
+        const data = jwt.verify(token, ACCESS_SECRET);
+        req.user = data;
+        next();
+    } catch {
+        res.status(401).json({ error: "توکن نامعتبر" });
+    }
+};
